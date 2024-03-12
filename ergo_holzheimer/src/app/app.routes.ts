9a172454b73e25272c0ce9d742c7a21e74cfc8d6
Max Köhler <max.koehler@dcs-fuerth.de>
2024-03-12 09:31:18 +0100

    Header & Routing

--- ergo_holzheimer/src/app/app.routes.ts
+++ ergo_holzheimer/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: '', component: HomepageComponent },
+  { path: 'home', component: HomepageComponent },
+];
+
+export const routingComponents = [
+  HomepageComponent,
+  ];
