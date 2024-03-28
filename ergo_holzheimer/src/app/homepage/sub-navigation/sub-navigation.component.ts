b51fcefd7dd96a07987d41ee026cd5b3e42ed36d
Max Köhler <max.koehler@dcs-fuerth.de>
2024-03-28 16:31:58 +0100

    first step theming

--- ergo_holzheimer/src/app/homepage/sub-navigation/sub-navigation.component.ts
+++ ergo_holzheimer/src/app/homepage/sub-navigation/sub-navigation.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sub-navigation',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './sub-navigation.component.html',
   styleUrl: './sub-navigation.component.scss'
 })
